a8a3bb200884dc1efabb0961ba0154b80b95b114
arch <arch@localhost.localdomain>
2014-07-29 08:59:28 -0600

    Interfaz para agregar auals

---
+++ generador-horarios-php/interfaz/administrar_aulas/agregar/js/funciones.js
@@ -0,0 +1,33 @@
+$(function(){
+    
+    $(document).on("click",".formulario",function(){        
+        $("#resultado").html("");
+    });
+
+    $(document).on("change",".form-control",function(){        
+        $("#resultado").html("");
+    });
+    
+    $(document).on("click","#add",function(){
+        $.ajax({
+            type: "POST",
+            url: "./aAula.php",
+            data: {codigo: $('#codigo').val(),capacidad: $('#capacidad').val(),exclusiva: document.getElementById('exclusiva').checked},
+            success: function(datos){
+                datos = jQuery.parseJSON(datos);            
+                if(datos==="¡Aula Agregada!"){
+                    $("#resultado").html("<font color='green'>"+datos+"</font>");
+                }else{
+                    $("#resultado").html("<font color='red'>"+datos+"</font>");
+                }            
+            },
+            error:function(err){                                  
+                $("#resultado").html("Los datos no se pudieron enviar!");
+            }
+        });
+    });    
+    
+});
+
+
+
