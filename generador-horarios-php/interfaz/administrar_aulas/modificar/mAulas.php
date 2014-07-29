291de3bb1510fb72e8fa01a902a507931184aaea
arch <arch@localhost.localdomain>
2014-07-29 09:34:48 -0600

    Interfaz para modificar aulas

---
+++ generador-horarios-php/interfaz/administrar_aulas/modificar/mAulas.php
@@ -0,0 +1,30 @@
+<?php
+chdir(dirname(__FILE__));
+require_once 'config.php';
+require_once '../../../reglas_negocio/ManejadorSesion.php';
+chdir(dirname(__FILE__));
+require_once '../../../reglas_negocio/ManejadorAulas.php';
+chdir(dirname(__FILE__));
+
+ManejadorSesion::sec_session_start();
+
+if($_POST){
+    if($_POST['pk'] && $_POST['name'] && $_POST['value']){
+        $id = $_POST['pk'];
+        $campo = $_POST['name'];
+        $valor = $_POST['value'];
+        try{
+            ManejadorAulas::modificarAula($id,$campo, $valor);
+            $respuesta = array('status'=>'ok','msg'=>'¡Materia Modificada!');
+            print json_encode($respuesta);
+        }catch(Exception $ex){
+            $respuesta = array('status'=>'error','msg'=>$ex->getMessage());
+            print json_encode($respuesta);            
+        }       
+
+    }    
+    
+}
+
+
+?>
