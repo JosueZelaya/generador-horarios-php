cf39637de0d4cff2909ede82bec1a31f88aa4780
arch <arch@localhost.localdomain>
2014-07-30 06:03:37 -0600

    Se añadió un método para agregar desactivar departamentos, se llamo al método: desactivarDepartamento. Se añadió método para buscar departamentos.

--- generador-horarios-php/reglas_negocio/ManejadorDepartamentos.php
+++ generador-horarios-php/reglas_negocio/ManejadorDepartamentos.php
@@ -23,6 +23,11 @@ abstract class ManejadorDepartamentos {
         }
     }
     
+    public static function eliminarDepartamento($departamento){
+        $consulta = "UPDATE departamentos SET activo='f' WHERE id_depar='".$departamento->getId()."'";
+        conexion::consulta2($consulta);
+    }
+    
     public static function existe($departamento){
         $consulta = "SELECT COUNT(*) FROM departamentos WHERE nombre_depar='".$departamento->getNombre()."'";
         $respuesta = conexion::consulta2($consulta);
@@ -51,6 +56,27 @@ abstract class ManejadorDepartamentos {
             $depars[] = $depar;
         }
         return $depars;
+    }
+    
+    public static function getDepartamentosConPaginacion($pagina,$numeroResultados){
+        $depars = array();
+        $pagina = ($pagina-1)*$numeroResultados;
+        $sql_consulta = "SELECT * FROM departamentos WHERE activo='t' ORDER BY nombre_depar ASC LIMIT ".$numeroResultados." OFFSET ".$pagina;
+        $respuesta = conexion::consulta($sql_consulta);
+        while ($fila = pg_fetch_array($respuesta)){
+            $depars[] = new Departamento($fila['id_depar'],$fila['nombre_depar']);
+        }
+        return $depars;
+    }
+    
+    public static function buscarDepartamento($buscarComo){
+        $departamentos = array();
+        $consulta = "SELECT * FROM departamentos WHERE nombre_depar iLIKE '%$buscarComo%' AND activo='t' ORDER BY nombre_depar;";
+        $respuesta = Conexion::consulta($consulta);
+        while ($fila = pg_fetch_array($respuesta)){
+            $departamentos[] = new Departamento($fila['id_depar'],$fila['nombre_depar']);
+        }
+        return $departamentos;
     }
     
     public static function getIdDepar($nombre, $depars){
