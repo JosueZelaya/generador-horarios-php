8a19dc4cfd72165950d9417f4946db38ff6a8d0b
abs <abs@AbsArch>
2014-08-05 15:54:07 -0600

    se modifica css de index en /admin para no interferir con los iconos del filtro por hora

--- generador-horarios-php/interfaz/admin/index.php
+++ generador-horarios-php/interfaz/admin/index.php
@@ -35,7 +35,7 @@ chdir(dirname(__FILE__));
                 <div class="row">
                     <div class="col-lg-3">
                         <a href="../agrupaciones_crear/index.php" target="_blank">
-                            <span><i class="fa fa-cubes fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-cubes fa-lg"></i></span>
                             <!--<img width="140px" height="140px" src="../imagenes/placeholder1-3.jpg" class="img-circle">-->
                             <h2>Crear Agrupaciones</h2>
                             <p>Paso 1: Desde acá usted podrá crear agrupaciones de materias.
@@ -51,7 +51,7 @@ chdir(dirname(__FILE__));
                     <div class="col-lg-3">
                         <a href="../agrupaciones_datos/index.php" target="_blank">
                             <!--<img width="140px" height="140px" src="../imagenes/placeholder2.jpg" class="img-circle">-->
-                            <span><i class="fa fa-edit fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-edit fa-lg"></i></span>
                             <h2>Agrupaciones y Datos</h2>
                             <p>Paso 2: Una vez completado el paso anterior acá podrá indicar cuálos son los requerimientos
                                de cada agrupación: El número de alumnos que llevarán la materia materia, el tamaño de los grupos
@@ -63,7 +63,7 @@ chdir(dirname(__FILE__));
                     <div class="col-lg-3">
                         <a href="../agrupaciones_datos_especiales/index.php"  target="_blank">
                             <!--<img width="140px" height="140px" src="../imagenes/placeholder-datos-especiales.jpg" class="img-circle">-->
-                            <span><i class="fa fa-star fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-star fa-lg"></i></span>
                             <h2>Agrupaciones y Datos Especiales</h2>
                             <p>Puede agregar grupos de laboratorio y de discusión a sus materias.
                             </p>
@@ -73,7 +73,7 @@ chdir(dirname(__FILE__));
                     <div class="col-lg-3">
                         <a href="../agrupaciones_docentes/index.php" target="_blank">
                             <!--<img width="140px" height="140px" src="../imagenes/placeholder3-1.jpg" class="img-circle">-->
-                            <span><i class="fa fa-male fa-lg"></i><i class="fa fa-female fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-male fa-lg"></i><i class="fa fa-female fa-lg"></i></span>
                             <h2>Agrupaciones y docentes</h2>
                             <p>Paso 3: Una vez completado el paso 2, en esta sección podrá asignar los docentes que impartirán
                                cada grupo de una materia</p>
@@ -86,7 +86,7 @@ chdir(dirname(__FILE__));
                     <div class="col-lg-3">
                         <a href="../agrupaciones_materias/index.php" target="_blank">
                             <!--<img width="140px" height="140px" src="../imagenes/libros3.jpg" class="img-circle">-->
-                            <span><i class="fa fa-exchange fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-exchange fa-lg"></i></span>
                             <h2>Agrupaciones y Materias</h2>
                             <p>Se pueden añadir o quitar materias a las agrupaciones</p>
                         </a>                    
@@ -95,7 +95,7 @@ chdir(dirname(__FILE__));
                     <div class="col-lg-3">
                         <a href="../reservaciones_aulas/index.php" target="_blank">
                             <!--<img width="140px" height="140px" src="../imagenes/placeholder4.jpg" class="img-circle">-->
-                            <span><i class="fa fa-lock fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-lock fa-lg"></i></span>
                             <h2>Reservaciones de Aulas</h2>
                             <p>Se pueden hacer reservaciones en las aulas para que nos sean tomadas en cuenta en la generación
                             del nuevo horario.</p>
@@ -105,7 +105,7 @@ chdir(dirname(__FILE__));
                     <div class="col-lg-3">
                         <a href="../agrupaciones_aulas/index.php" target="_blank">
                             <!--<img width="140px" height="140px" src="../imagenes/placeholder-aulas.jpg" class="img-circle">-->
-                            <span><i class="fa fa-building fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-building fa-lg"></i></span>
                             <h2>Agrupaciones y Aulas</h2>
                             <p>Se puede definir en qué aulas se impartirán los grupos de determinada materia.</p>
                         </a>                    
@@ -114,7 +114,7 @@ chdir(dirname(__FILE__));
                     <div class="admin-users col-lg-3">
                         <a href="#admin_users_modal" data-toggle="modal">
                             <!--<img width="140px" height="140px" src="../imagenes/placeholder-usuarios.jpg" class="img-circle">-->
-                            <span><i class="fa fa-users fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-users fa-lg"></i></span>
                             <h2>Administrar Docentes y Usuarios</h2>
                             <p>Agregar, modificar y eliminar Docentes y Usuarios</p>
                         </a>                    
@@ -125,7 +125,7 @@ chdir(dirname(__FILE__));
                     <div class="admin-users col-lg-3">
                         <a href="#admin_materias_modal" data-toggle="modal">
                             <!--<img width="140px" height="140px" src="../imagenes/libros2.jpg" class="img-circle">-->
-                            <span><i class="fa fa-book fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-book fa-lg"></i></span>
                             <h2>Administrar Materias</h2>
                             <p>Agregar, modificar y eliminar Materias</p>
                         </a>                    
@@ -134,7 +134,7 @@ chdir(dirname(__FILE__));
                     <div class="admin-users col-lg-3">
                         <a href="#admin_aulas_modal" data-toggle="modal">
                             <!--<img width="140px" height="140px" src="../imagenes/aula.jpg" class="img-circle">-->
-                            <span><i class="fa fa-university fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-university fa-lg"></i></span>
                             <h2>Administrar Aulas</h2>
                             <p>Agregar, modificar y eliminar Aulas</p>
                         </a>                    
@@ -143,7 +143,7 @@ chdir(dirname(__FILE__));
                     <div class="admin-users col-lg-3">
                         <a href="#admin_departamentos_modal" data-toggle="modal">
                             <!--<img width="140px" height="140px" src="../imagenes/departamentos.jpg" class="img-circle">-->
-                            <span><i class="fa fa-sitemap fa-lg"></i></span>
+                            <span class="index_icon"><i class="fa fa-sitemap fa-lg"></i></span>
                             <h2>Administrar Departamentos</h2>
                             <p>Agregar, modificar y eliminar Departamentos</p>
                         </a>                    
